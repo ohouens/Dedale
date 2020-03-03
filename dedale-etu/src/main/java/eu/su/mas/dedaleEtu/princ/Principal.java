6a218ee79b4466d85a2df3106a19201879754fda
pamelaosuna <pamelaosuna@10.64.6.52>
2020-03-03 12:43:21 +0100

    added golem and binaryTree graphs

--- dedale-etu/src/main/java/eu/su/mas/dedaleEtu/princ/Principal.java
+++ dedale-etu/src/main/java/eu/su/mas/dedaleEtu/princ/Principal.java
@@ -290,19 +290,20 @@ public class Principal {
 //		/*********
 //		 * GOLEM
 //		 *********/
-//		//1) Get the container where the agent will appear
-//		c = containerList.get(ConfigurationFile.LOCAL_CONTAINER2_NAME);
-//		Assert.assertNotNull("This container does not exist",c);
-//		
-//		//2) Give the name of your agent, MUST be the same as the one given in the entities file.
-//		agentName="Golem";
-//		
-//		//3) If you want to give specific parameters to your agent, add them here
-//		Object [] entityParameters={"My parameters"};
-//		
-//		//4) Give the class name of your agent to let the system instantiate it
+		//1) Get the container where the agent will appear
+		c = containerList.get(ConfigurationFile.LOCAL_CONTAINER2_NAME);
+		Assert.assertNotNull("This container does not exist",c);
+		
+		//2) Give the name of your agent, MUST be the same as the one given in the entities file.
+		agentName="Golem";
+		
+		//3) If you want to give specific parameters to your agent, add them here
+		Object [] entityParameters={"My parameters"};
+		
+		//4) Give the class name of your agent to let the system instantiate it
 //		ag=createNewDedaleAgent(c, agentName, DummyWumpusShift.class.getName(), entityParameters);
-//		agentList.add(ag);	
+		ag=createNewDedaleAgent(c, agentName, DummyMovingAgent.class.getName(), entityParameters);
+		agentList.add(ag);	
 //		
 //		
 //		/*********
@@ -384,7 +385,8 @@ public class Principal {
 		/*********
 		 * AGENT Explo1
 		 *********/
-//		//1) Get the container where the agent will appear
+		/*
+		//1) Get the container where the agent will appear
 		c = containerList.get(ConfigurationFile.LOCAL_CONTAINER2_NAME);
 		Assert.assertNotNull("This container does not exist",c);
 		
@@ -397,10 +399,11 @@ public class Principal {
 		//4) Give the class name of your agent to let the system instantiate it
 		ag=createNewDedaleAgent(c, agentName, ExploreMultiAgent.class.getName(), entityParametersExplo1);//
 		agentList.add(ag);
-		
+		*/
 		/*********
 		 * AGENT Explo2
 		 *********/
+		
 		//1) Get the container where the agent will appear
 		c = containerList.get(ConfigurationFile.LOCAL_CONTAINER2_NAME);
 		Assert.assertNotNull("This container does not exist",c);
@@ -415,7 +418,7 @@ public class Principal {
 		ag=createNewDedaleAgent(c, agentName, ExploreMultiAgent.class.getName(), entityParametersExplo2);//ExploreSoloAgent
 		
 		agentList.add(ag);
-//		
+		
 		
 		/*********
 		 * GOLEM
