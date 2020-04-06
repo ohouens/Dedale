63fc5e31d82675d7f0276fd83b2771ccd6b46cd7
ohouens <rohouens@yahoo.fr>
2020-04-06 18:17:55 +0200

    adding of merge in mapMaker

--- mapCreation.py
+++ mapCreation.py
@@ -1,3 +1,5 @@
+import copy as cp
+
 class MapCreation():
     def __init__(self):
         self.nodeCursor = 0
@@ -12,6 +14,7 @@ class MapCreation():
         if(stick >= 0):
             self.origin = stick
         else:
+            self.nodeCursor+=1
             self.origin = self.nodeCursor
 
     def clean(self, border):
@@ -27,8 +30,41 @@ class MapCreation():
         for eID, n1, n2 in self.edges:
             print("ae e{0}\t{1} {2}".format(eID, n1, n2))
 
+    def merge(self, b1, b2):
+        c1, i1 = b1
+        c2, i2 = b2
+        n1 = c1[i1]
+        n2 = c2[i2]
+        self.nodeCursor+=1
+        n3 = self.nodeCursor
+        self.nodes.add(n3)
+        dejaVu = set()
+        dejaVu.add(n3)
+        for e in cp.deepcopy(self.edges):
+            if(n1==e[1] or n1==e[2]):
+                self.edges.remove(e)
+                dejaVu.add(e[1])
+                dejaVu.add(e[2])
+                if(n1==e[1]):
+                    self.joint(n3, e[2])
+                else:
+                    self.joint(e[1], n3)
+            if(n2==e[1] or n2==e[2]):
+                self.edges.remove(e)
+                if(n2==e[1]):
+                    self.joint(n3, e[2])
+                else:
+                    self.joint(e[1], n3)
+        self.nodes.remove(n1)
+        self.nodes.remove(n2)
+        c1[i1] = n3
+        c2[i2] = n3
+        return c1, c2
 
     def joint(self, n1, n2):
+        for e, t1, t2 in self.edges:
+            if(t1==n2 and t2==n1):
+                return
         self.edges.add((self.edgeCursor, n1, n2))
         self.edgeCursor += 1
 
@@ -72,9 +108,8 @@ class MapCreation():
 
 if __name__ == "__main__":
     map = MapCreation()
-    map.straight()
-    map.square()
-    map.triangle()
-    map.bridge()
-    map.circle()
+    c1 = map.square()
+    c2 = map.square()
+    c1, c2 = map.merge((c1,1), (c2,0))
+    c1, c2 = map.merge((c1,2), (c2,3))
     map.build()
